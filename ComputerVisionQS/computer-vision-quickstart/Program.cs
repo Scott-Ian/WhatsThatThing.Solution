5eb710e39de2e79979185a913f20fc258c0dbff7
Ian Scott <chesnekov@gmail.com>
2020-08-24 11:35:04 -0700

    add Read URL file for identifying text in the image, also includes code for listing out identified landmarks, color schemes, and a few others

--- ComputerVisionQS/computer-vision-quickstart/Program.cs
+++ ComputerVisionQS/computer-vision-quickstart/Program.cs
@@ -23,11 +23,14 @@ namespace computer_vision_quickstart
 
         //URL of Image to analyze
         private const string ANALYZE_URL_IMAGE = "https://www.thesprucepets.com/thmb/3-ISVJpCrp9TUfeRdH1mfzJlHGg=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/golden-retriever-puppy-in-grass-923135452-5c887d4146e0fb00013365ba.jpg";
+        private const string READ_TEXT_URL_IMAGE = "https://media.npr.org/assets/img/2016/04/17/handwritten-note_wide-941ca37f3638dca912c8b9efda05ee9fefbf3147.jpg?s=1400";
         static void Main(string[] args)
         {
             // Instantiate client
             ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
             AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
+            // Extract text from a URL image using the Read API
+            ReadFileUrl(client, READ_TEXT_URL_IMAGE).Wait();
         }
         
         // Authenticates the computer vision client
@@ -118,6 +121,92 @@ namespace computer_vision_quickstart
             Console.WriteLine($"Has racy content: {results.Adult.IsRacyContent} with confidence {results.Adult.RacyScore}");
             Console.WriteLine();
 
+            // Identifies the color scheme
+            Console.WriteLine("Color Scheme:");
+            Console.WriteLine("Is black and white?: " + results.Color.IsBWImg);
+            Console.WriteLine("Accent color: " + results.Color.DominantColorBackground);
+            Console.WriteLine("Dominant foreground color: " + results.Color.DominantColorForeground);
+            Console.WriteLine("Dominant colors: " + string.Join(",", results.Color.DominantColors));
+            Console.WriteLine();
+
+            // Detects the image types
+            Console.WriteLine("Image Type:");
+            Console.WriteLine("Clip Art Type: " + results.ImageType.ClipArtType);
+            Console.WriteLine("Line Drawing Type: " + results.ImageType.LineDrawingType);
+            Console.WriteLine();
+
+            // Identifies celeberties in image, if any
+            Console.WriteLine("Celeberties: ");
+            foreach (var catergory in results.Categories)
+            {
+                if (catergory.Detail?.Celeberties != null)
+                {
+                    foreach (var celeb in catergory.Detail.Celeberties)
+                    {
+                        Console.WriteLine($"{celeb.Name} with confidence {celeb.Confidence} at location {celeb.FaceRectangle.Left}, " + $"{celeb.FaceRectangle.Top}, {celeb.FaceRectangle.Height}, {celeb.FaceRectangle.Width}");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            // Popular landmarks in image, if any
+            Console.WriteLine("Landmarks:");
+            foreach (var category in results.Categories)
+            {
+                if (category.Detail?.Landmarks != null)
+                {
+                    foreach (var landmark in category.Detail.Landmarks)
+                    {
+                        Console.WriteLine($"{landmark.Name} with confidence {landmark.Confidence}");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+        }
+
+        /*
+        * READ FILE - URL
+        * Extracts text
+        */
+        public static async Task ReadFileUrl(ComputerVisionClient client, string urlFile)
+        {
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("READ FILE FROM URL");
+            Console.WriteLine();
+
+            // Read text from URL
+            var textHeaders = await client.ReadAsync(urlFile, language: "en");
+            // After request, get operation location
+            string operationLocation = textHeaders.OperationLocation;
+            Thread.Sleep(2000);
+
+            // Retrieve the URI where the extracted text will be stored from the Operation-Location Header
+            // We only need the ID and not the full URL
+            const int numberOfCharsInOperationId = 36;
+            string operationId = operationLocation.SubString(operationLocation.Length - numberOfCharsInOperationId);
+
+            // Extract text
+            ReadOperationResults results;
+            Console.WriteLine($"Extracting text from URL file {Path.GetFileName(urlFile)}...");
+            Console.WriteLine();
+            do
+            {
+                results = await client.GetReadResultAsync(Guid.Parse(operationId));
+            }
+            while ((results.Status == OperationStatusCodes.Running || results.Status == OperationStatusCodes.NotStarted));
+
+            // Display the found text
+            Console.WriteLine();
+            vArgIterator textUrlFileResults = results.AnalyzeResult.ReadResults;
+            foreach (ReadResult page in textUrlFileResults)
+            {
+                foreach (Line line in page.Lines)
+                {
+                    Console.WriteLine(line.Text);
+                }
+            }
+            Console.WriteLine();
         }
 
     }
