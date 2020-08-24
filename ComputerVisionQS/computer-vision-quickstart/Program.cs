8a6c67bb21f9d53d89c57dd000cfb529fcdf799a
Ian Scott <chesnekov@gmail.com>
2020-08-24 10:36:03 -0700

    add AnalyzeImageUrl method to Program.cs for analyzing an image via url

--- ComputerVisionQS/computer-vision-quickstart/Program.cs
+++ ComputerVisionQS/computer-vision-quickstart/Program.cs
@@ -17,10 +17,15 @@ namespace computer_vision_quickstart
 {
     class Program
     {
+        // Environment Variable Injection
         static string subscriptionKey = ComputerVisionKey;
         static string endpoint = ComputerVisionEndpoint;
+
+        //URL of Image to analyze
+        private const string ANALYZE_URL_IMAGE = "https://www.thesprucepets.com/thmb/3-ISVJpCrp9TUfeRdH1mfzJlHGg=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/golden-retriever-puppy-in-grass-923135452-5c887d4146e0fb00013365ba.jpg";
         static void Main(string[] args)
         {
+            // Instantiate client
             ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
         }
         
@@ -31,5 +36,29 @@ namespace computer_vision_quickstart
                 { Endpoint = endpoint };
             return client;
         }
+
+        /* 
+        * ANALYZE IMAGE - URL IMAGE
+        * Extracts captions, categories, tags, objects, faces, racy/adult content,
+        * brands, celebrities, landmarks, color scheme, and image types.
+        */
+        public static async Task AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+        {
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("ANALYZE IMAGE - URL");
+            Console.WriteLine();
+
+            //Creating a list that defines the features to be extracted from the image.
+            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            {
+                VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+                VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+                VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
+                VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
+                VisualFeatureTypes.Objects
+            };
+
+        }
+
     }
 }
