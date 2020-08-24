90acdec127e1336eacd449b57105689f15f6371b
Ian Scott <chesnekov@gmail.com>
2020-08-24 10:14:24 -0700

    adds Computer Vision quick start project as a template, installs cognitiveservices computervision package, and inlcudes hidden environmental variables

---
+++ ComputerVisionQS/computer-vision-quickstart/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComupterVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
+using ComputerVisionQS;
+
+// ComputerVisionKey
+// ComputerVisionEndpoint
+
+namespace computer_vision_quickstart
+{
+    class Program
+    {
+        static string subscriptionKey = ComputerVisionKey;
+        static string endpoint = ComputerVisionEndpoint;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
