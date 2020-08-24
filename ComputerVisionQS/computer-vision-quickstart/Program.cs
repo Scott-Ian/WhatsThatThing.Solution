12baf6bcac7a393e121072f666e7981afa92fd52
Ian Scott <chesnekov@gmail.com>
2020-08-24 13:39:35 -0700

    updates ComputerVisionQS to fix errors preventing build. Comments out read text section, as it seems to not be working. Updates GeneralNotes.md with a short note
    Co-authored-by: bwhite2140@outlook.com <bwhite2140@outlook.com>
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- ComputerVisionQS/computer-vision-quickstart/Program.cs
+++ ComputerVisionQS/computer-vision-quickstart/Program.cs
@@ -22,7 +22,7 @@ namespace computer_vision_quickstart
         static string endpoint = EnvironmentVariables.ComputerVisionEndpoint;
 
         //URL of Image to analyze
-        private const string ANALYZE_URL_IMAGE = "https://www.thesprucepets.com/thmb/3-ISVJpCrp9TUfeRdH1mfzJlHGg=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/golden-retriever-puppy-in-grass-923135452-5c887d4146e0fb00013365ba.jpg";
+        private const string ANALYZE_URL_IMAGE = "https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg";
         private const string READ_TEXT_URL_IMAGE = "https://media.npr.org/assets/img/2016/04/17/handwritten-note_wide-941ca37f3638dca912c8b9efda05ee9fefbf3147.jpg?s=1400";
         static void Main(string[] args)
         {
@@ -30,7 +30,7 @@ namespace computer_vision_quickstart
             ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
             AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
             // Extract text from a URL image using the Read API
-            ReadFileUrl(client, READ_TEXT_URL_IMAGE).Wait();
+            // ReadFileUrl(client, READ_TEXT_URL_IMAGE).Wait();
         }
         
         // Authenticates the computer vision client
@@ -169,45 +169,45 @@ namespace computer_vision_quickstart
         * READ FILE - URL
         * Extracts text
         */
-        public static async Task ReadFileUrl(ComputerVisionClient client, string urlFile)
-        {
-            Console.WriteLine("-------------------------------");
-            Console.WriteLine("READ FILE FROM URL");
-            Console.WriteLine();
+        // public static async Task ReadFileUrl(ComputerVisionClient client, string urlFile)
+        // {
+        //     Console.WriteLine("-------------------------------");
+        //     Console.WriteLine("READ FILE FROM URL");
+        //     Console.WriteLine();
 
-            // Read text from URL
-            var textHeaders = await client.ReadAsync(urlFile, language: "en");
-            // After request, get operation location
-            string operationLocation = textHeaders.OperationLocation;
-            Thread.Sleep(2000);
+        //     // Read text from URL
+        //     var textHeaders = await client.ReadAsync(urlFile, language: "en");
+        //     // After request, get operation location
+        //     string operationLocation = textHeaders.OperationLocation;
+        //     Thread.Sleep(2000);
 
-            // Retrieve the URI where the extracted text will be stored from the Operation-Location Header
-            // We only need the ID and not the full URL
-            const int numberOfCharsInOperationId = 36;
-            string operationId = operationLocation.SubString(operationLocation.Length - numberOfCharsInOperationId);
+        //     // Retrieve the URI where the extracted text will be stored from the Operation-Location Header
+        //     // We only need the ID and not the full URL
+        //     const int numberOfCharsInOperationId = 36;
+        //     string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
 
-            // Extract text
-            ReadOperationResults results;
-            Console.WriteLine($"Extracting text from URL file {Path.GetFileName(urlFile)}...");
-            Console.WriteLine();
-            do
-            {
-                results = await client.GetReadResultAsync(Guid.Parse(operationId));
-            }
-            while ((results.Status == OperationStatusCodes.Running || results.Status == OperationStatusCodes.NotStarted));
+        //     // Extract text
+        //     ReadOperationResult results;
+        //     Console.WriteLine($"Extracting text from URL file {Path.GetFileName(urlFile)}...");
+        //     Console.WriteLine();
+        //     do
+        //     {
+        //         results = await client.GetReadResultAsync(Guid.Parse(operationId));
+        //     }
+        //     while ((results.Status == OperationStatusCodes.Running ||
+        //         results.Status == OperationStatusCodes.NotStarted));
 
-            // Display the found text
-            Console.WriteLine();
-            var textUrlFileResults = results.AnalyzeResult.ReadResults;
-            foreach (ReadResult page in textUrlFileResults)
-            {
-                foreach (Line line in page.Lines)
-                {
-                    Console.WriteLine(line.Text);
-                }
-            }
-            Console.WriteLine();
-        }
-
+        //     // Display the found text
+        //     Console.WriteLine();
+        //     var textUrlFileResults = results.AnalyzeResult.ReadResults;
+        //     foreach (ReadResult page in textUrlFileResults)
+        //     {
+        //         foreach (Line line in page.Lines)
+        //         {
+        //             Console.WriteLine(line.Text);
+        //         }
+        //     }
+        //     Console.WriteLine();
+        // }
     }
 }
