1ac5751d46e973992dedc3c20813b42f4d27cddc
Ian Scott <chesnekov@gmail.com>
2020-08-24 11:00:33 -0700

    add code for face detection, brands, adult content , objects, image tags, and categories

--- ComputerVisionQS/computer-vision-quickstart/Program.cs
+++ ComputerVisionQS/computer-vision-quickstart/Program.cs
@@ -27,6 +27,7 @@ namespace computer_vision_quickstart
         {
             // Instantiate client
             ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+            AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
         }
         
         // Authenticates the computer vision client
@@ -57,6 +58,65 @@ namespace computer_vision_quickstart
                 VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
                 VisualFeatureTypes.Objects
             };
+            
+            Console.WriteLine($"Analyzing the image {Path.GetFileName(imageUrl)}");
+            Console.WriteLine();
+            // Analyze inputted image URL
+            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, features);
+
+            // Summarizes the image content
+            Console.WriteLine("Summary:");
+            foreach (var caption in results.Description.Captions)
+            {
+                Console.WriteLine($"{caption.Text} with confidence {caption.Confidence}");
+            }
+            Console.WriteLine();
+
+            // Display categories the image is divided into
+            Console.WriteLine("Categories:");
+            foreach (var category in results.Categories)
+            {
+                Console.WriteLine($"{category.Name} with confidence {category.Score}");
+            }
+            Console.WriteLine();
+
+            // Image tags and their confidence score
+            Console.WriteLine("Tags: ");
+            foreach ( var tag in results.Tags)
+            {
+                Console.WriteLine($"{tag.Name} {tag.Confidence}");
+            }
+            Console.WriteLine();
+
+            //Objects
+            Console.WriteLine("Objects: ");
+            foreach (var obj in results.Objects)
+            {
+                Console.WriteLine($"{obj.ObjectProperty} with confidence {obj.Confidence} at location {obj.Rectangle.X}, " + $"{obj.Rectangle.X + obj.Rectangle.W}, {obj.Rectangle.Y}, {obj.Rectangle.Y + obj.Rectangle.H}");
+            }
+            Console.WriteLine();
+
+            // Well-known brands
+            Console.WriteLine("Brands: ");
+            foreach (var brand in results.Brands)
+            {
+                Console.WriteLine($"Logo of {brand.Name} with confidence {brand.Confidence} at location {brand.Rectangle.X}, " + $"{brand.Rectangle.X + brand.Rectangle.W}, {brand.Rectangle.Y}, {brand.Rectangle.Y + brand.Rectangle.H}");
+            }
+            Console.WriteLine();
+
+            // Returns detected faces and their attributes
+            Console.WriteLine("Faces:");
+            foreach (var face in results.Faces)
+            {
+                Console.WriteLine($"A {face.Gender} of age {face.Age} at location {face.FaceRectange.Left}, "+ $"{face.FaceRectangel.Left}, {face.FaceRectange.Top + face.FaceRectangle.Width}, " + $"{face.FaceRectangle.Top + face.FaceRectangle.Height}");
+            }
+            Console.WriteLine();
+
+            // Adult, racy, or gory content, if any:
+            Console.WriteLine("Adult:");
+            Console.WriteLine($"Has adult content: {results.Adult.IsAdultContent} with confidence { results.Adult.AdultScore}");
+            Console.WriteLine($"Has racy content: {results.Adult.IsRacyContent} with confidence {results.Adult.RacyScore}");
+            Console.WriteLine();
 
         }
 
