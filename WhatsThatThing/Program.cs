d5ebbcdecc3d0dbffd1c693de8393bd6a3f0ecdf
Spencer Moody <spencer.moody@outlook.com>
2020-08-25 14:03:08 -0700

    Clean up file tree
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>
    Co-authored-by: Ian Scott <chesnekov@gmail.com>
    Co-authored-by: Ben White <bwhite2140@outlook.com>
    Co-authored-by: Megan Hepner <megan.hepner@gmail.com>

--- WhatsThatThing/Program.cs
+++ WhatsThatThing/Program.cs
@@ -7,37 +7,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
-using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System.Threading;
-using WhatsThatThing;
 
 namespace WhatsThatThing
 {
     public class Program
     {
-        // public static string subscriptionKey = EnvironmentVariables.ComputerVisionKey;
-        // public static string endpoint = EnvironmentVariables.ComputerVisionEndpoint;
-
-        // public static ComputerVisionClient Authenticate(string endpoint, string key)
-        // {
-        //     ComputerVisionClient client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
-        //         { Endpoint = endpoint };
-        //     return client;
-        // }
-        
-        //URL of Image to analyze
-        // private const string ANALYZE_URL_IMAGE = "https://i.ytimg.com/vi/MPV2METPeJU/maxresdefault.jpg";
-        // private const string READ_TEXT_URL_IMAGE = "https://media.npr.org/assets/img/2016/04/17/handwritten-note_wide-941ca37f3638dca912c8b9efda05ee9fefbf3147.jpg?s=1400";
-        
         public static void Main(string[] args)
         {
-
-            // ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
-            // AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
-
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -47,135 +24,5 @@ namespace WhatsThatThing
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-        
-        // Authenticates the computer vision client
-        
-
-        /* 
-        * ANALYZE IMAGE - URL IMAGE
-        * Extracts captions, categories, tags, objects, faces, racy/adult content,
-        * brands, celebrities, landmarks, color scheme, and image types.
-        */
-        public static async Task AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
-        {
-
-            // Console.WriteLine("-------------------------------");
-            // Console.WriteLine("ANALYZE IMAGE - URL");
-            // Console.WriteLine();
-
-            //Creating a list that defines the features to be extracted from the image.
-            
-            List<VisualFeatureTypes> features = new List<VisualFeatureTypes>()
-            {
-                VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
-                VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
-                VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
-                VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
-                VisualFeatureTypes.Objects
-            };
-            
-            Console.WriteLine($"Analyzing the image {Path.GetFileName(imageUrl)}");
-            Console.WriteLine();
-
-            // Analyze inputted image URL
-            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, features);
-
-            // Summarizes the image content
-            Console.WriteLine("Summary:");
-            foreach (var caption in results.Description.Captions)
-            {
-                Console.WriteLine($"{caption.Text} with confidence {caption.Confidence}");
-            }
-            Console.WriteLine();
-
-            // Display categories the image is divided into
-            Console.WriteLine("Categories:");
-            foreach (var category in results.Categories)
-            {
-                Console.WriteLine($"{category.Name} with confidence {category.Score}");
-            }
-            Console.WriteLine();
-
-            // Image tags and their confidence score
-            Console.WriteLine("Tags: ");
-            foreach ( var tag in results.Tags)
-            {
-                Console.WriteLine($"{tag.Name} {tag.Confidence}");
-            }
-            Console.WriteLine();
-
-            //Objects
-            Console.WriteLine("Objects: ");
-            foreach (var obj in results.Objects)
-            {
-                Console.WriteLine($"{obj.ObjectProperty} with confidence {obj.Confidence} at location {obj.Rectangle.X}, " + $"{obj.Rectangle.X + obj.Rectangle.W}, {obj.Rectangle.Y}, {obj.Rectangle.Y + obj.Rectangle.H}");
-            }
-            Console.WriteLine();
-
-            // Well-known brands
-            Console.WriteLine("Brands: ");
-            foreach (var brand in results.Brands)
-            {
-                Console.WriteLine($"Logo of {brand.Name} with confidence {brand.Confidence} at location {brand.Rectangle.X}, " + $"{brand.Rectangle.X + brand.Rectangle.W}, {brand.Rectangle.Y}, {brand.Rectangle.Y + brand.Rectangle.H}");
-            }
-            Console.WriteLine();
-
-            // Returns detected faces and their attributes
-            Console.WriteLine("Faces:");
-            foreach (var face in results.Faces)
-            {
-                Console.WriteLine($"A {face.Gender} of age {face.Age} at location {face.FaceRectangle.Left}, "+ $"{face.FaceRectangle.Left}, {face.FaceRectangle.Top + face.FaceRectangle.Width}, " + $"{face.FaceRectangle.Top + face.FaceRectangle.Height}");
-            }
-            Console.WriteLine();
-
-            // Adult, racy, or gory content, if any:
-            Console.WriteLine("Adult:");
-            Console.WriteLine($"Has adult content: {results.Adult.IsAdultContent} with confidence { results.Adult.AdultScore}");
-            Console.WriteLine($"Has racy content: {results.Adult.IsRacyContent} with confidence {results.Adult.RacyScore}");
-            Console.WriteLine();
-
-            // Identifies the color scheme
-            Console.WriteLine("Color Scheme:");
-            Console.WriteLine("Is black and white?: " + results.Color.IsBWImg);
-            Console.WriteLine("Accent color: " + results.Color.DominantColorBackground);
-            Console.WriteLine("Dominant foreground color: " + results.Color.DominantColorForeground);
-            Console.WriteLine("Dominant colors: " + string.Join(",", results.Color.DominantColors));
-            Console.WriteLine();
-
-            // Detects the image types
-            Console.WriteLine("Image Type:");
-            Console.WriteLine("Clip Art Type: " + results.ImageType.ClipArtType);
-            Console.WriteLine("Line Drawing Type: " + results.ImageType.LineDrawingType);
-            Console.WriteLine();
-
-            // Identifies celeberties in image, if any
-            Console.WriteLine("Celeberties: ");
-            foreach (var catergory in results.Categories)
-            {
-                if (catergory.Detail?.Celebrities != null)
-                {
-                    foreach (var celeb in catergory.Detail.Celebrities)
-                    {
-                        Console.WriteLine($"{celeb.Name} with confidence {celeb.Confidence} at location {celeb.FaceRectangle.Left}, " + $"{celeb.FaceRectangle.Top}, {celeb.FaceRectangle.Height}, {celeb.FaceRectangle.Width}");
-                    }
-                }
-            }
-            Console.WriteLine();
-
-            // Popular landmarks in image, if any
-            Console.WriteLine("Landmarks:");
-            foreach (var category in results.Categories)
-            {
-                if (category.Detail?.Landmarks != null)
-                {
-                    foreach (var landmark in category.Detail.Landmarks)
-                    {
-                        Console.WriteLine($"{landmark.Name} with confidence {landmark.Confidence}");
-                    }
-                }
-            }
-            Console.WriteLine();
-
-        }
     }
 }
